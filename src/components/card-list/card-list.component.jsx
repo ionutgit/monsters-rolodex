9797f1831ab4c651525aa9f293ec1371fb01ae9d
ionutgit <ionut_chirita@yahoo.com>
2024-02-08 10:37:58 +0200

    CardList Component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        return (
+            <div>Hello, I'm card list component</div>
+        )
+    }
+}
+
+export default CardList
